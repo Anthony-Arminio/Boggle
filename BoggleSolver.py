e05f4effab0120322e6a886a1d07d50523cee187
Sal Arminio <tonyarminio.1@gmail.com>
2023-09-08 21:39:38 -0400

    basic agent running

--- BoggleSolver.py
+++ BoggleSolver.py
@@ -1,3 +1,32 @@
 import numpy as np
+import math
 
-print("Hello World")
+class Agent():
+    def __init__(self, board):
+        self.board = board
+        self.boardSize = len(board)
+    
+    def run(self):
+        words = open('words_alpha.txt','r')
+        solutionWords = self.findSolutionWords(words)
+        print(solutionWords)
+        print(len(solutionWords))
+    
+    def findSolutionWords(self, words):
+        solutionWords = []
+        for word in words:
+            word = word.strip()
+            for char in word:
+                for tile in self.board:
+                    if char == tile.lower():
+                        solutionWords.append(word)
+                        print("Appended " + word + ".")
+                        break
+        return solutionWords
+
+
+
+boggleSolver = Agent([
+    "X"
+])
+boggleSolver.run()
