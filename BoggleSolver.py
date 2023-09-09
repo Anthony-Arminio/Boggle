f73ab7dcb1df8cf5685f24c1f3dbf92442754cd3
Sal Arminio <tonyarminio.1@gmail.com>
2023-09-09 10:31:42 -0400

    one letter working

--- BoggleSolver.py
+++ BoggleSolver.py
@@ -3,8 +3,9 @@ import math
 
 class Agent():
     def __init__(self, board):
-        self.board = board
-        self.boardSize = len(board)
+        self.board = self.convertBoard(board)
+        self.boardSize = len(self.board)
+        self.boardWidth = math.floor(math.sqrt(self.boardSize))
     
     def run(self):
         words = open('words_alpha.txt','r')
@@ -16,17 +17,49 @@ class Agent():
         solutionWords = []
         for word in words:
             word = word.strip()
-            for char in word:
-                for tile in self.board:
-                    if char == tile.lower():
-                        solutionWords.append(word)
-                        print("Appended " + word + ".")
-                        break
+            if self.isPlayable(0, word, -1, []):
+                solutionWords.append(word)
+                print("Appended " + word + ".")
         return solutionWords
+    
+    def isPlayable(self, location, word, charIndex, usedTiles):
+        if charIndex == -1:
+            for tile in range(len(self.board)):
+                if self.isPlayable(tile, word, charIndex + 1, usedTiles):
+                    return True
+                
+            return False
+        else:
+            if self.board[location].lower() != word[charIndex] or (location in usedTiles):
+                return False
+            usedTiles.append(location)
+            return True
+        
+    def row(self, location):
+        return math.floor(location / self.boardWidth) + 1
+    
+    def column(self, location):
+        return location % self.boardWidth + 1
 
 
 
-boggleSolver = Agent([
-    "X"
-])
+
+
+            
+    
+    def convertBoard(self, board):
+        convertedBoard = []
+        for tile in range(len(board)):
+            if board[tile].isupper():
+                if tile + 1 < len(board) and board[tile + 1].islower():
+                    convertedBoard.append(board[tile] + board[tile + 1])
+                else:
+                    convertedBoard.append(board[tile])
+        print(convertedBoard)
+        return convertedBoard
+                
+
+boggleSolver = Agent(
+    "AAAAAAAAAAAAAAAAAAAAAAAAA"
+)
 boggleSolver.run()
